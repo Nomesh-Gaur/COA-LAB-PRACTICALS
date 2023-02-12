756f2aee09d8b754b7bb264e7eadafd54ab45e98
Nomesh-Gaur <113971266+Nomesh-Gaur@users.noreply.github.com>
2023-02-12 19:47:17 +0530

    Create FULL-SUBTRACTOR.cpp

---
+++ FULL-SUBTRACTOR.cpp
@@ -0,0 +1,29 @@
+//FULL SUBTRACTOR
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void Full_Subtractor(int A,int B,int Bin){
+   
+    int Difference = (A ^ B) ^ Bin;
+       
+    
+    int A1 = not(A);
+       
+   
+    int B_Out = A1 & Bin | A1 & B | B & Bin;
+   
+ 
+    cout<<"Difference = "<<Difference<<endl;
+    cout<<"B-Out = "<<B_Out<<endl;
+   
+  }
+
+int main() {
+    int A ;
+    int B ;
+    int Bin ;
+    cin>>A>>B>>Bin;
+    Full_Subtractor(A, B, Bin);
+    return 0;
+}
